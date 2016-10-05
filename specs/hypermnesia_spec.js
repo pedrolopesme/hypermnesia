408cc39754f246e348d36d882806f72e2efeef05
Pedro Mendes <pedrolopesme@gmail.com>
2016-10-04 23:09:16 -0300

    Removing code duplication on tests.

--- specs/hypermnesia_spec.js
+++ specs/hypermnesia_spec.js
@@ -5,7 +5,9 @@ describe("Hypermnesia", function(){
     var cache = null;
 
     beforeEach(function(){
-        cache = null;
+        cache = new hypermnesia(function(key){
+            return Math.floor((Math.random() * 100000) + 1);
+        });
     });
 
     it("Must receive a function in it's creation", function(){
@@ -20,10 +22,6 @@ describe("Hypermnesia", function(){
     });
 
     it("Should return item from cache it was cached", function(){
-        cache = new hypermnesia(function(key){
-            return Math.floor((Math.random() * 100000) + 1);
-        });
-
         var itemValue = cache.get(1);
         expect(cache.get(1)).toBe(itemValue);
     });
@@ -36,9 +34,6 @@ describe("Hypermnesia", function(){
     });
 
     it("Should allow remove a item by its key from cache", function(){
-        cache = new hypermnesia(function(key){
-            return Math.floor((Math.random() * 100000) + 1);
-        });
         var item1 = cache.get(1);
         expect(cache.get(1)).toBe(item1);
 
@@ -47,17 +42,11 @@ describe("Hypermnesia", function(){
     });
 
     it("Should behave properly when trying to remove from cache a non-existent element", function(){
-        cache = new hypermnesia(function(key){
-            return Math.floor((Math.random() * 100000) + 1);
-        });
         cache.remove(1);
     });
 
 
     it("Should allow remove all elements from cache", function(){
-        cache = new hypermnesia(function(key){
-            return Math.floor((Math.random() * 100000) + 1);
-        });
         var item1 = cache.get(1);
         var item2 = cache.get(2);
         var item3 = cache.get(3);
